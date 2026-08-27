8e0cb80b6fd68a1455db48bb97cfd7e2c96988e1
svit32323 <90389406+svit32323@users.noreply.github.com>
2026-08-27 10:34:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Bot prijavljen kot ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
